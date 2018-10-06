cd27519463d212cd4a5c71b79e3d087951e45c52
helpthewalrus <alinatylets@gmail.com>
2018-10-06 17:50:39 +0300

    task_10_commit_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let longest = 0;
+  const map = {};
+  array.forEach(function(number) {
+    map[number] = true;
+  });
+  for (let number in map) {
+    let current = Number(number),
+        count = 1;
+    while (map[current + 1]) {
+      count++;
+      current++;
+    }
+  longest = Math.max(longest, count);
+  }
+ return longest;
 }
